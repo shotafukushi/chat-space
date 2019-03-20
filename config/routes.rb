c63b36ac785a57b1e649e0406994d92a3ea0eade
shotafukushi <sfukushi@sfukushinoMacBook-Pro.local>
2019-03-20 15:15:21 +0900

    flashmessage

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-
-  get 'users/update'
-
   devise_for :users
   root to: "chat_spaces#index"
   resource :chat_spaces
-
+  resources :users, only: [:edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
