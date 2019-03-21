5f0bad674ba03064b0433d3512597e0e47bd54fb
shotafukushi <sfukushi@sfukushinoMacBook-Pro.local>
2019-03-21 15:38:11 +0900

    create message controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
   devise_for :users
-  root to: "chat_spaces#index"
+  root to: "groups#index"
   resource :chat_spaces
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
