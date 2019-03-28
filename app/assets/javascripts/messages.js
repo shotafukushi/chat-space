398639d13722c673f2a5f950513004c8907c2138
shotafukushi <sfukushi@sfukushinoMacBook-Pro.local>
2019-03-28 18:31:32 +0900

    javascript非同期処理

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,63 @@
+$(function() {
+  //メッセージ表示のHTML生成
+  function buildHTML(message){
+    img = message.image ? `<img src = "${message.image}" class = "lower-message__image">`:"";
+
+    var html = `<div class="message">
+                 <div class="upper-message">
+                  <div class="upper-message__user-name">
+                    ${ message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                      ${message.created_at}
+                  </div>
+                 </div>
+                 <div class="lower-message>
+                     <p class="lower-message__content">
+                       ${ message.content }
+                     </p>
+                     ${img}
+                 </div>
+                </div>`;
+
+  console.log(message.user_name);
+  console.log(message.created_at);
+  console.log(message.content);
+
+  return html;
+  }
+
+  //メッセージの非同期通信
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+
+    console.log(url);
+
+
+    $.ajax({
+     url: url,
+     type: "POST",
+     data: formData,
+     dataType: 'json',
+     processData: false,
+     contentType: false
+    })
+    .done(function(data) {
+      /*data.forEach(function(message){*/
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('#new_message')[0].reset()
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
+        $('.form__submit').prop('disabled',false);
+        $('.form__message').val("");
+    })
+    .fail(function() {
+      alert('errdsfgfgsor');
+      $('.submit-btn').prop('disabled', false);
+    })
+    $('#new_message')[0].reset();
+    return false;
+  })
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,16 +4,25 @@ class MessagesController < ApplicationController
  def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+       format.html
+       format.json  { @new_message = Message.where('id > ?', params[:message][:id]) }
+    end
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+       respond_to do |format|
+         format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
+         format.json
+      end
     else
-      @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
-      render :index
+      respond_to do |format|
+        @messages = @group.messages.includes(:user)
+        flash.now[:alert] = 'メッセージを入力してください。'
+        render :index
+      end
     end
   end
 
